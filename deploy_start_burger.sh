cd5628ce61b7c41a76c5a8cfdf53db04ac0dd5f2
balancy <balancy@gmail.com>
2021-12-02 23:05:09 +0000

    Add sending information about deploy via deployment script to rollbar API

--- deploy_start_burger.sh
+++ deploy_start_burger.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
 git pull
-poetry install
-python manage.py migrate
-python manage.py collectstatic --no-input
+poetry install || exit "$?"
+python manage.py migrate || exit "$?"
+python manage.py collectstatic --no-input || exit "$?"
 
 npm install
 sudo npm install -g parcel@latest || exit "$?"
-parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
+parcel build bundles-src/index.js --dist-dir bundles --public-url="./" || exit "$."
 
 sudo systemctl restart star-burger.service  || exit "$?"
 
+curl -X POST https://api.rollbar.com/api/1/deploy \
+     -H "X-Rollbar-Access-Token: "$(awk -F'=' '/^ROLLBAR_ACCESS_TOKEN/ { print $2}' .env)"" \
+     -H "Content-Type: application/json" \
+     -d $'{
+        "environment": "production",
+        "revision": "'"$(git rev-parse HEAD)"'",
+        "rollbar_username": "'"$(whoami)"'",
+        "local_username": "'"$(git config user.name)"'",
+        "comment": "deploy"
+}'
+echo
 echo "Project deployed successfully"
 
